a674302ffb3861ca92074662dcbf939009186f3f
Yuto Omachi <youomachi@gmail.com>
2020-06-23 11:09:55 +0900

    HistoryCell and HistoryVC basic components created.

--- Score Tracker/HistoryCell.swift
+++ Score Tracker/HistoryCell.swift
@@ -11,43 +11,49 @@ import Stevia
 
 class HistoryCell: UITableViewCell {
     
-    var numCol: Int?
+    var numCol: Int? {
+        didSet {
+            if labels.isEmpty {
+                for i in 0...(numCol ?? 0) {
+                    let label = UILabel()
+                    label.tag = i+10
+                    addSubview(label)
+                    label.translatesAutoresizingMaskIntoConstraints = false
+                    labels.append(label)
+                }
+                
+                configureLabels()
+            }
+        }
+    }
+    
     var labels = [UILabel]()
 
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
         self.backgroundColor = UIColor.backgroundColor
-        
-        for i in 0...(numCol ?? 0) {
-            let label = UILabel()
-            label.tag = i+10
-            addSubview(label)
-            label.translatesAutoresizingMaskIntoConstraints = false
-            labels.append(label)
-        }
-        
-        configureLabels()
+    
     }
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
+
     
     func configureLabels() {
         let width: Double = Double(100)/Double(labels.count)
         var currLeft: Double = 0.01
 
-        
         for label in labels {
-            if label.tag != 0 {
-                label.height(100%).centerVertically().left((currLeft)%).width((width)%)
-                currLeft += width
-                label.text = "1"
-                label.textAlignment = .center
-                label.layer.borderColor = UIColor.gray.cgColor
-                label.layer.borderWidth = 0.5
+            label.height(100%).centerVertically().left((currLeft)%).width((width)%)
+            currLeft += width
+            label.textAlignment = .center
+            label.layer.borderColor = UIColor.gray.cgColor
+            label.layer.borderWidth = 0.5
+            
+            if label.tag == 10 {
+                label.backgroundColor = .cellColor
             }
         }
     }
--- Score Tracker/HistoryVC.swift
+++ Score Tracker/HistoryVC.swift
@@ -12,6 +12,8 @@ import Stevia
 class HistoryVC: UIViewController {
     
     let tableView = UITableView()
+    var players = ["Aya", "Sara", "Tomo", "Ted", "Yuto", "Abrakadabra SesamiStreet"]
+    var gameScore = [[0,4,0,1,0,0],[0,0,0,2,0,1],[1,0,0,0,0,1],[0,0,0,2,0,1],[0,0,0,2,0,1],[0,0,0,2,0,1]]
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -34,13 +36,18 @@ class HistoryVC: UIViewController {
 
 extension HistoryVC: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 5
+        return gameScore.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         if let cell = tableView.dequeueReusableCell(withIdentifier: "history", for: indexPath) as? HistoryCell{
-            cell.numCol = 5
-            cell.
+            cell.numCol = players.count
+            cell.labels[0].text = "\(indexPath.row+1)"
+            for (i, label) in cell.labels.enumerated() {
+                if i != 0 {
+                    label.text = "\(gameScore[indexPath.row][i-1])"
+                }
+            }
             return cell
         }
         
@@ -48,10 +55,50 @@ extension HistoryVC: UITableViewDelegate, UITableViewDataSource {
     }
     
     func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
-        return { let view = UIView()
-            view.height(30%).width(100%).centerVertically().centerHorizontally()
-            view.backgroundColor = .cyan
-            return view
-        }()
+        let view = UIView()
+        view.height(tableView.rowHeight).width(100%)
+        view.backgroundColor = .cellColor
+        
+        let width: Double = Double(100)/Double(players.count+1)
+        var currLeft: Double = 0.01
+        
+        for i in 0...players.count {
+            let label = UILabel()
+            view.subviews {
+                label
+            }
+            label.height(100%).centerVertically().left((currLeft)%).width((width)%)
+            currLeft += width
+            label.textAlignment = .center
+            label.layer.borderColor = UIColor.gray.cgColor
+            label.layer.borderWidth = 0.5
+            label.adjustsFontSizeToFitWidth = true
+            label.numberOfLines = 0
+            
+            if i != 0 {
+                label.text = players[i-1]
+            }
+        }
+        return view
+    }
+    
+    func tableView(_ tableView: UITableView, trailingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = UIContextualAction(style: .destructive, title: "Delete"){
+            [weak self] (action, view, nil) in
+            self?.deleteTapped(indexPath: indexPath)
+        }
+        let config = UISwipeActionsConfiguration(actions: [delete])
+        config.performsFirstActionWithFullSwipe = true
+        return config
+    }
+    
+    func deleteTapped(indexPath: IndexPath) {
+        let ac = UIAlertController(title: "Delete this round?", message: "Once you deleted, the score cannot be restored", preferredStyle: .alert)
+        ac.addAction(UIAlertAction(title: "Delete", style: .destructive){ [weak self] _ in
+            self?.gameScore.remove(at: indexPath.row)
+            self?.tableView.reloadData()
+        })
+        ac.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        present(ac, animated: true)
     }
 }
