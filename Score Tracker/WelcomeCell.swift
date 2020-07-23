77af57e31bdc17596b9459e6f0177ba0b4ea14bd
Yuto Omachi <youomachi@gmail.com>
2020-07-23 12:09:14 +0900

    Added all three Welcome Images

--- Score Tracker/WelcomeCell.swift
+++ Score Tracker/WelcomeCell.swift
@@ -7,8 +7,10 @@
 //
 
 import UIKit
+import Stevia
 
 class WelcomeCell: UICollectionViewCell {
+    let imgView = UIImageView()
     
     override init(frame: CGRect) {
         super.init(frame: frame)
@@ -20,6 +22,11 @@ class WelcomeCell: UICollectionViewCell {
     }
     
     func configureCell() {
-        
+        self.subviews{
+            imgView
+        }
+        imgView.size(100%)
+        imgView.contentMode = .scaleAspectFit
+        imgView.backgroundColor = UIColor(red: 250/255, green: 250/255, blue: 250/255, alpha: 1)
     }
 }
--- Score Tracker/WelcomeVC.swift
+++ Score Tracker/WelcomeVC.swift
@@ -11,34 +11,80 @@ import Stevia
 
 class WelcomeVC: UICollectionViewController {
     
+    var numPages = 4
+    
+    var startButton: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .navigationColor
+        return button
+    }()
+    
+    fileprivate lazy var pageControl: UIPageControl = {
+        let pc = UIPageControl()
+        pc.currentPage = 0
+        pc.numberOfPages = numPages
+        pc.currentPageIndicatorTintColor = .red
+        pc.pageIndicatorTintColor = UIColor(red: 249/255, green: 207/255, blue: 224/255, alpha: 1)
+        return pc
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-//        setCollectionView()
         setCollectionView()
+        
+        view.subviews {
+            startButton
+            pageControl
+        }
+        setConstraints()
     }
     
     func setCollectionView() {
-//        collectionView.dataSource = self
-//        collectionView.delegate = self
         collectionView.backgroundColor = .white
         collectionView.register(WelcomeCell.self, forCellWithReuseIdentifier: "Cell")
         collectionView.isPagingEnabled = true
+        collectionView.showsHorizontalScrollIndicator = false
     }
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return 4
+        return numPages
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath)
-        if indexPath.item % 2 == 0 {
-            cell.backgroundColor = .green
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as? WelcomeCell else { return UICollectionViewCell() }
+        
+        switch indexPath.item {
+        case 0:
+            cell.imgView.image = UIImage(named: "Welcome1")
+        case 1:
+            cell.imgView.image = UIImage(named: "Welcome2")
+        case 2:
+            cell.imgView.image = UIImage(named: "Welcome3")
+        default:
+            return cell
         }
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
         return 0
+    }
+    
+    override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        let x = targetContentOffset.pointee.x
+        
+        pageControl.currentPage = Int(x/view.frame.width)
+    }
+    
+    func setConstraints() {
+        startButton.left(10%).width(80%).bottom(5%).height(10%)
+        startButton.setTitle("Start Now", for: .normal)
+        startButton.addTarget(self, action: #selector(startButtonTapped), for: .touchUpInside)
+        pageControl.left(10%).width(80%).bottom(15%).height(10%)
+    }
+    
+    @objc func startButtonTapped() {
+        print("start button tapped")
     }
 
 }
